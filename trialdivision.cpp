838c5acd5ebf33592c55f9868b0ff27fdaf33268
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2016-11-18 20:21:25 +0100

    just forgot to add the src

---
+++ trialdivision.cpp
@@ -0,0 +1,30 @@
+//
+//  trialdivision.cpp
+//  Factoring
+//
+//  Created by Edvard Ahlsén on 18/11/16.
+//  Copyright © 2016 Edvard Ahlsén. All rights reserved.
+//
+
+
+#include "trialdivision.h"
+
+std::vector<mpz_class>* trialdivision(mpz_class &N) {
+    std::vector<mpz_class> *factors = new std::vector<mpz_class>();
+    for (auto p : PI) {
+        if (p > sqrt(N)) { break; }
+        while(N % p == 0) {
+            factors->push_back(mpz_class(p));
+            N /= p;
+        }
+    }
+    if (N > 1) {
+        if (mpz_probab_prime_p (N.get_mpz_t(), 15)) {
+            factors->push_back(N);
+        } else {
+            throw "fail";
+        }
+    }
+    
+    return factors;
+}
---
+++ trialdivision.h
@@ -0,0 +1,22 @@
+//
+//  trialdivision.h
+//  Factoring
+//
+//  Created by Edvard Ahlsén on 18/11/16.
+//  Copyright © 2016 Edvard Ahlsén. All rights reserved.
+//
+
+#ifndef trialdivision_h
+#define trialdivision_h
+
+#include <vector>
+#include <stdio.h>
+#include <gmpxx.h>
+
+
+#include "constants.h"
+
+std::vector<mpz_class>* trialdivision(mpz_class &N);
+
+#endif /* trialdivision_h */
+
